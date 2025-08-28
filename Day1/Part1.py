d6919a47d4d45a924403ea143c4afc887dc61480
rose <rose.dangol23@gmail.com>
2025-08-28 13:59:30 +0545

    day1-Task1

---
+++ Day1/Part1.py
@@ -0,0 +1,23 @@
+#Length & Indexing
+
+quote = "Be Good.Do Good"
+
+quoteLength = len(quote)
+print(quoteLength)
+
+print(quote[0])
+print(quote[quoteLength-1])
+
+#Concatenation
+
+firstName="Rose"
+lastName="Dangol"
+fullName= firstName+" "+lastName
+print(fullName)
+
+#Repetition
+repeat="Python!" *10
+print(repeat)
+
+# for x in range(10):
+#     print("Python\t")
