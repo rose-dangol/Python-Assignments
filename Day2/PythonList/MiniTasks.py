516472a135d43983408caf764d99849cacce65fc
rose <rose.dangol23@gmail.com>
2025-08-29 22:00:41 +0545

    day2

---
+++ Day2/PythonList/MiniTasks.py
@@ -0,0 +1,9 @@
+# 1. Create a list of 10 numbers and get the maximum value using max() .
+# 2. Create a list of 10 numbers and get the minimum value using min() .
+# 3. Create a list of 10 numbers and find the sum using sum() .
+# 4. Check if all elements in a list are True using all() .
+# 5. Check if any element in a list is True using any() .
+# 6. Get the length of a list using len() .
+# 7. Convert a string "apple" into a list of characters using list() .
+# 8. Create a list from a tuple (1, 2, 3, 4) .
+# 9. Create a list from a range of numbers from 1 to 5 using list(range()) .
--- Day2/PythonList/listMethods.py
+++ Day2/PythonList/listMethods.py
@@ -1,5 +1,73 @@
 numbers = [5, 2, 9, 1, 5, 6]
 fruits = ["apple", "banana", "cherry"]
 
+#1. Append "orange" to fruits
 fruits.append("orange")
 
+# 2. Insert "kiwi" at index 1 in fruits .
+fruits.insert(1,"kiwi")
+print(fruits)
+
+# 3. Remove "banana" from fruits .
+fruits.remove("banana")
+print(fruits)
+
+# 4. Pop the last item from numbers .
+numbers.pop(len(numbers)-1)
+print(numbers)
+
+# 5. Clear all elements from a copy of fruits .
+fruitsCopy=list(fruits)
+print(fruitsCopy)
+fruitsCopy.clear()
+print(fruitsCopy)
+
+# 6. Sort numbers in ascending order.
+numbers.sort()
+print(numbers)
+
+# 7. Sort numbers in descending order.
+numbers.sort(reverse=True)
+print(numbers)
+
+# 8. Reverse fruits without sorting.
+left=0
+right=len(numbers)-1
+
+while(left<right):
+    temp= numbers[left]
+    numbers[left]=numbers[right]
+    numbers[right]=temp
+    left = left+1
+    right=right-1
+print(numbers)
+
+# 9. Sort fruits alphabetically.
+fruits.sort()
+print(fruits)
+
+# 10. Count how many times 5 appears in numbers.
+count=0
+for x in numbers:
+    if x ==5:
+        count=count+1
+print(count)
+
+# 11. Find the index of "cherry" in fruits .
+print(fruits)
+length=len(fruits)
+i =0
+index=-1
+while i<length:
+    if fruits[i]=="cherry":
+        index=i
+    i=i+1
+
+print(f"Index of cherry: {index}")
+
+# 12. Create a shallow copy of numbers and modify it — check if the original changes.
+numbersCopy = numbers.copy()
+numbersCopy.append(23)
+
+print(numbersCopy)
+print(numbers)
